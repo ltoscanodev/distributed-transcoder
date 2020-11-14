f47849033c4aa162536a6dcecea12949ba334ce4
Luis Augusto Toscano <dev.ltoscano@gmail.com>
2020-11-14 18:32:12 -0300

    Criação dos projetos

---
+++ distributed-transcoder/merge-svc/src/test/java/dev/ltoscano/transcoder/DistributedTranscoderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ltoscano.transcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedTranscoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ distributed-transcoder/transcoder-svc/src/main/java/dev/ltoscano/transcoder/DistributedTranscoderApplication.java
@@ -0,0 +1,13 @@
+package dev.ltoscano.transcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTranscoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedTranscoderApplication.class, args);
+	}
+
+}
